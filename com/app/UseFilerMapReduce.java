a80341941f70447941398bbf9ca120927dfce507
mangesh687 <mangeshpakhale2016@gmail.com>
2025-10-10 15:42:56 +0530

    interviwe question

--- com/app/UseFilerMapReduce.java
+++ com/app/UseFilerMapReduce.java
@@ -8,6 +8,8 @@ public static void main(String[] args) {
 	
 	List<Integer> sc=Arrays.asList(1,2,3,4,5,6);
 	Integer sc1=sc.stream().filter(x->x%2==0).map(x->x*x).reduce(0, (a,b)->a+b);
+	Integer sc2=sc.stream().filter(x->x%2==0).map(x->x*x).reduce(0, Integer::sum);
 	System.out.println(sc1);
+	System.out.println(sc2);
 }
 }
