2dd468cefae48d3c23100e1e576a96b91bf6f7c4
mangesh687 <mangeshpakhale2016@gmail.com>
2025-10-10 15:31:43 +0530

    java 8 code

---
+++ com/app/UseFilerMapReduce.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class UseFilerMapReduce {
+public static void main(String[] args) {
+	
+	List<Integer> sc=Arrays.asList(1,2,3,4,5,6);
+	Integer sc1=sc.stream().filter(x->x%2==0).map(x->x*x).reduce(0, (a,b)->a+b);
+	System.out.println(sc1);
+}
+}
